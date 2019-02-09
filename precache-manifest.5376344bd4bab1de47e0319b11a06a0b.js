a6e9ef63855e1d250b1146674a2cb5a9dde77a40
Karim Abdel Hamid <karimah@aucegypt.edu>
2019-02-09 21:45:25 +0200

    Updates

---
+++ precache-manifest.5376344bd4bab1de47e0319b11a06a0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "18fda4996589afd0978b",
+    "url": "/Oak.js/static/js/runtime~main.18fda499.js"
+  },
+  {
+    "revision": "7fb805cd670a08eb71f3",
+    "url": "/Oak.js/static/js/main.7fb805cd.chunk.js"
+  },
+  {
+    "revision": "746187d81ff8a7f437c5",
+    "url": "/Oak.js/static/js/1.746187d8.chunk.js"
+  },
+  {
+    "revision": "21505c9bb38533bb79a0a3fcb4f1d6d9",
+    "url": "/Oak.js/index.html"
+  }
+];
