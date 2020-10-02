725815d546ffe3eb95e5574b8e33d136f274c226
Donn <me@donn.website>
2020-10-02 13:38:45 +0200

    Updates

---
+++ precache-manifest.cea19c9f222c44e23e30ecfb8ca118f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3719ed405ffdf71e87d416f6f74ce79a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57e0e8eecf1743f99ea3",
+    "url": "./static/js/2.222d987c.chunk.js"
+  },
+  {
+    "revision": "e63617f8fbe5c0d54ed2b70fdb2ff677",
+    "url": "./static/js/2.222d987c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74e817d704fe384f1d0",
+    "url": "./static/js/main.99c5d37a.chunk.js"
+  },
+  {
+    "revision": "0680ced136b6dcb92768",
+    "url": "./static/js/runtime-main.d70132fe.js"
+  }
+]);
